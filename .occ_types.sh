e38b0531ecf9388c96c14c1069502f4c2f14902f
dimstunt <mr.d.fagot@gmail.com>
2019-01-31 17:46:16 +0300

    Add lowercase, filter hidden files
    1) Added type conversion to lowercase for better merging
    2) Removed hidden files from selection

--- .occ_types.sh
+++ .occ_types.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # Sort and count number of occurrence of types
 
 function occ_types() {
-  find . -maxdepth 1 -type f | rev | cut -d "." -f  1 | grep -v "/" | rev | sort | uniq -c | sort
+  find . -maxdepth 1 -type f | grep -v "\.\/\." | sed 's/\.\///' | grep "\." | rev | cut -d "." -f  1 | rev | sort | tr '[:upper:]' '[:lower:]' | uniq -c | sort -r
 }
